de5346893fe2d975838c94f07e228a2c798e2f84
@hmed <spb6926@gmail.com>
2020-11-13 12:28:15 +0100

    solution

---
+++ twoMax2.js
@@ -0,0 +1,6 @@
+function twoMax2(arr1,arr2){
+    let arr = arr1.concat(arr2);
+    arr.sort((a,b)=>b-a);
+    return [arr[1],arr[0]];
+}
+//console.log(twoMax2([2, 5, 8, 5, -5, 13],[6, 4, -8, 45, 0])) ===>[13,45]
