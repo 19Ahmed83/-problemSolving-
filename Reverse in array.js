879365d1d8bca65d940dd8b62f0e7ddc8644de15
@hmed <spb6926@gmail.com>
2020-12-05 07:28:11 +0100

    solution

--- Reverse in array.js
+++ Reverse in array.js
@@ -1,11 +1,12 @@
-function app(n){
-    let arr=[];
+function app(n) {
+    let arr = [];
     let last;
     do {
         last = n % 10;
         arr.push(last);
         n = (n - last) / 10;
-    }while(n>0)
+    } while (n > 0)
     return arr
 }
+
 console.log(app(123))//[3,2,1]
