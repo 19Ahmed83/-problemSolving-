ba63c3d044d569957fa34b104a02644b2e50937a
@hmed <spb6926@gmail.com>
2020-12-05 07:42:46 +0100

    solution

--- True or false.js
+++ True or false.js
@@ -1,13 +1,14 @@
-function app(n){
+function app(n) {
     let last;
-    do{
-        last=n%10;
-        n=(n-last)/10;
-        if(last===2){
+    do {
+        last = n % 10;
+        n = (n - last) / 10;
+        if (last === 2) {
             return true;
         }
-    }while(n>0)
+    } while (n > 0)
     return false
 
 }
+
 console.log(app(1520))//true
