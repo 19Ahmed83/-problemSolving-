6070c95f42b84fcfc2141de026bdcee75439a2aa
@hmed <spb6926@gmail.com>
2021-04-02 11:15:12 +0200

    solution

---
+++ stringConcatination.js
@@ -0,0 +1,7 @@
+let myNoun = "dog";
+let myAdjective = "big";
+let myVerb = "ran";
+let myAdverb = "quickly";
+
+let wordBlanks = "The " + myNoun + ' ' + myAdjective + ' ' + myVerb +' ' + myAdverb;
+console.log(wordBlanks);
