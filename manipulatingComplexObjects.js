6577348c7dda136de49787e4f11ee9e8838b9079
@hmed <spb6926@gmail.com>
2021-04-05 08:23:20 +0200

    manipulatingComplexObjects

---
+++ manipulatingComplexObjects.js
@@ -0,0 +1,13 @@
+let myMusic = [{
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: [
+        "CD",
+        "8T",
+        "LP"
+    ],
+    gold: true
+}]
+myMusic.push({artist: 'Duft Pank', title: 'Homework', release_year: 1997, formats: ['CD', 'Cassete', 'LP']})
+console.log(myMusic)
