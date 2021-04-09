4abc8b85a5ef8b4fc503cac250be84bc47537a70
@hmed <spb6926@gmail.com>
2021-04-09 12:00:34 +0200

    deleteAndPushWithSplice().js

---
+++ deleteAndPushWithSplice().js
@@ -0,0 +1,6 @@
+function htmlColors(arr) {
+    return arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond')
+
+}
+
+console.log(htmlColors(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']))
