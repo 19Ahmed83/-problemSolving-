9c40bda6506342cd77e2bde087d6bf94d5ab15f4
@hmed <spb6926@gmail.com>
2021-04-09 12:20:14 +0200

    copyAnArrayWithTheSpreadOperator.js

---
+++ copyAnArrayWithTheSpreadOperator.js
@@ -0,0 +1,10 @@
+function copyMashine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        newArr.push([...arr]);
+        num--;
+    }
+    return newArr;
+}
+
+console.log(copyMashine([true, false, null], 3))
