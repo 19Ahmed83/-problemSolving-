4e18b27b57cf1685c1e4d54a29231497cea7dd63
@hmed <spb6926@gmail.com>
2021-04-10 15:55:07 +0200

    IteratingOverTheKeysOfAnObjectUsingTheSor ... inStatement.js

---
+++ IteratingOverTheKeysOfAnObjectUsingTheSor ... inStatement.js
@@ -0,0 +1,23 @@
+function countOnline(usersObj) {
+    // Only change code below this line
+    let count = 0;
+    for (let user in usersObj) {
+        if (usersObj[user].online === true) {
+            count++
+        }
+
+    }
+    return count;
+}
+
+console.log(countOnline({
+    Alan: {
+        online: false
+    },
+    Jeff: {
+        online: true
+    },
+    Sarah: {
+        online: false
+    }
+}))
