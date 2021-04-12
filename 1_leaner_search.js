a6d99f53f52a0c28ea217ac7b52832366e3513d8
@hmed <spb6926@gmail.com>
2021-04-12 20:59:08 +0200

    fibonnachiNumbers.js

--- 1_leaner_search.js
+++ 1_leaner_search.js
@@ -1,7 +1,8 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-
+let count=0;
 function test(arr, item) {
     for (let i = 0; i < arr.length; i++) {
+        count +=1
         if (arr[i] === item) {
             return i
         }
@@ -9,4 +10,5 @@ function test(arr, item) {
     return null
 }
 
-console.log(test(arr, 6))
+console.log(test(arr, 8))
+console.log('count =', count)
---
+++ fibonnachiNumbers.js
@@ -0,0 +1,7 @@
+const fibonnachi = (n) => {
+    if (n === 1 || n === 2) {
+        return 1
+    }
+    return fibonnachi(n - 1) + fibonnachi(n - 2)
+}
+console.log(fibonnachi(16))
