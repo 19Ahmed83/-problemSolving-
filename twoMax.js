84ca8c618891d6a6603dc7d9ab1a25d474f23124
@hmed <spb6926@gmail.com>
2021-04-14 10:11:28 +0200

    overrideInheritedMethods.js

--- twoMax.js
+++ twoMax.js
@@ -1,6 +1,7 @@
-function twoMax(arr1,arr2){
+function twoMax(arr1, arr2) {
     let max1 = Math.max(...arr1);
     let max2 = Math.max(...arr2);
-    return [max1,max2]
+    return [max1, max2]
 }
-//console.log(twoMax([2, 5, 8, 5, -5, 13],[6, 4, -8, 45, 0])) ===>[13,45]
+
+console.log(twoMax([2, 5, 8, 5, -5, 13], [6, 4, -8, 45, 0]))
