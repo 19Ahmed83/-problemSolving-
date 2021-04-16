cd523b70e20908d3123ae7da8c908f84fffa64db
@hmed <spb6926@gmail.com>
2021-04-16 15:34:08 +0200

    returnPartOfAnArrayUsingTheSliceMethod.js

---
+++ returnPartOfAnArrayUsingTheSliceMethod.js
@@ -0,0 +1,11 @@
+let inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+
+function sliceArray(anim, beginSlice, endSlice) {
+    return inputAnim.slice(beginSlice, endSlice)
+}
+
+function anim() {
+
+}
+
+console.log(sliceArray(anim, 1, 3))
