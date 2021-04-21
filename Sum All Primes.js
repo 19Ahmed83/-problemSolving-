a214c2dd8a24df3829e37c968d1dba1867a63cf1
@hmed <spb6926@gmail.com>
2021-04-21 15:16:46 +0200

    Sum All Primes.js

---
+++ Sum All Primes.js
@@ -0,0 +1,20 @@
+const sumPrime = (num) => {
+    let sum = 0;
+
+    function isPrime(i) {
+        for (let k = 2; k < i; k++) {
+            if (i % k === 0) {
+                return false
+            }
+        }
+        return true;
+    }
+
+    for (let i = 2; i <= num; i++) {
+        if (isPrime(i)) {
+            sum = sum + i;
+        }
+    }
+    return sum;
+}
+console.log(sumPrime(10))
