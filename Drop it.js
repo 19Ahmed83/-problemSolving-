01364757318d8bc0b46ef9cc1506dc7c00cddcd4
@hmed <spb6926@gmail.com>
2021-04-22 16:34:58 +0200

    Drop it.js

---
+++ Drop it.js
@@ -0,0 +1,9 @@
+function dropElements(arr, func) {
+    while (!func(arr[0])) {
+        arr.shift()
+    }
+    return arr;
+}
+
+console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;
+}))
